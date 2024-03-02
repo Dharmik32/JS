7d36dc7c7d784e029aaed0eab3e01958dd33a2f5
dharmik32 <dharmikchavda07@gmail.com>
2024-03-02 17:58:27 +0530

    Commit for mini project

---
+++ 07Notification/main.js
@@ -0,0 +1,10 @@
+let close = document.querySelector(".close-btn");
+let n = document.querySelector(".notification-bar");
+
+function show() {
+  n.style.display = "block";
+}
+
+function closes() {
+  n.style.display = "none";
+}
